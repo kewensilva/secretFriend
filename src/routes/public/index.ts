d6ec7f4672a4d12dd0e0fa9d5a02be668650da29
KewenSilva <kewen.macedo@gmail.com>
2025-11-15 20:52:25 -0300

    feat: configuration of routes public and private in project

--- src/routes/public/index.ts
+++ src/routes/public/index.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 const routes = Router();
 
 routes.get('/ping', (req, res) => {
-    res.json({ ping: true })
+    res.json(`Aplicação está funcionando corretamente na Rota Ping!`)
 })
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -7,25 +7,22 @@ import { requestInterceptor } from './utils/requestInterceptor';
 import routes from './routes/public/index';
 
 const app = express();
-app.use(express.json());        
+app.use(express.json());
 app.use(cors());
 
 app.all('/{*any}', requestInterceptor);
 app.use('/public', routes);
-
 app.use(express.urlencoded({ extended: true }));
 
 const startServer = (port: number, server: http.Server) => {
-    server.listen(port, ()=> {
+    server.listen(port, () => {
         console.log(`runnning server at PORT ${port}`);
-        
     })
 }
 const devServer = http.createServer(app);
-    if(process.env.NODE_ENV === 'production') {
-
-    }
-    else {
-        const serverPort: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
-        startServer(serverPort, devServer);
-    }
+if (process.env.NODE_ENV === 'production') {
+}
+else {
+    const serverPort: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+    startServer(serverPort, devServer);
+}
